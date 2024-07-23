891600ef18edf007d0c50548ce16c54995c964fb
Rain Babauta <rainbabauta1@gmail.com>
2024-07-22 20:22:29 -0700

    add attack animations

--- characters/base/base_character.gd
+++ characters/base/base_character.gd
@@ -1,15 +1,46 @@
-extends CharacterBody2D
+class_name BaseCharacter extends CharacterBody2D
 
-const SPEED = 300.0
+enum States {IDLE, RUN}
+
+const ACCELERATION = 3000
+const FRICTION = 3000
+const MAX_SPEED = 200
+
+var state = States.IDLE
+
+@onready var animation_player: AnimationPlayer = %AnimationPlayer
 
 
 func _physics_process(delta: float) -> void:
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	move(delta)
+	attack()
+
+
+func apply_friction(amount: float) -> void:
+	if velocity.length() > amount:
+		velocity -= velocity.normalized() * amount
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity = Vector2.ZERO
+
+
+func apply_movement(amount: Vector2) -> void:
+	velocity += amount
+	velocity = velocity.limit_length(MAX_SPEED)
+
+
+func attack() -> void:
+	if Input.is_action_just_pressed("primary_attack"):
+		animation_player.play()
+
+
+func move(delta: float):
+	var input_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+
+	if input_vector == Vector2.ZERO:
+		state = States.IDLE
+		apply_friction(FRICTION * delta)
+	else:
+		state = States.RUN
+		apply_movement(input_vector * ACCELERATION * delta)
 
 	move_and_slide()
