a763cba81ae9038e0d1c28e2341e17f5e884d8c4
Rain Babauta <rainbabauta1@gmail.com>
2026-01-27 20:57:37 -0800

    setup main scene and connect hiring screen to gm

--- engine/managers/game_manager.gd
+++ engine/managers/game_manager.gd
@@ -6,11 +6,18 @@ var free_agent_pool: Array[int] = []
 @export var entity_manager: EntityManager
 @export var quest_system: QuestSystem
 @export var company_system: CompanySystem
+@export var hiring_screen: HiringScreen
+@export var management_hub: Control
 
 
 func _ready() -> void:
+	initialize_game()
+
 	quest_system.quest_assigned.connect(company_system.on_quest_assigned)
 	quest_system.quest_resolved.connect(company_system.on_quest_resolved)
+
+	hiring_screen.setup(entity_manager, company_system, self, free_agent_pool)
+	hiring_screen.hiring_complete.connect(_on_hiring_complete)
 
 
 func initialize_game() -> void:
@@ -29,3 +36,11 @@ func initialize_game() -> void:
 	var company_entity = entity_manager.create_entity()
 	company_entity.add_component("CompanyComponent", CompanyComponent.new())
 	player_company_id = company_entity.id
+
+
+func _on_hiring_complete() -> void:
+	hiring_screen.visible = false
+	management_hub.visible = true
+
+	for i in range(3):
+		quest_system.generate_quest()
---
+++ ui/management_hub.gd
@@ -0,0 +1,5 @@
+class_name ManagementHub extends Control
+
+const ADVENTURER_CARD_SCENE = preload(
+	"res://ui/ui_components/adventurer_card.tscn"
+)
