5ef89df99c68bce207abadb551daa51c4357b6d4
Rain Babauta <rainbabauta1@gmail.com>
2026-01-28 17:46:26 -0800

    set up management hub scene tree

--- ui/management_hub.gd
+++ ui/management_hub.gd
@@ -1,5 +1,13 @@
 class_name ManagementHub extends Control
 
+@onready var generate_quest_button: Button = %GenerateQuestButton
+@onready var quest_list: VBoxContainer = %QuestList
+@onready var company_gold_label: Label = %CompanyGoldLabel
+@onready var active_quest_panel: PanelContainer = %ActiveQuestPanel
+@onready var active_quest_label: Label = %ActiveQuestLabel
+@onready var resolve_quest_button: Button = %ResolveQuestButton
+@onready var roster_list: VBoxContainer = %RosterList
+
 const ADVENTURER_CARD_SCENE = preload(
 	"res://ui/ui_components/adventurer_card.tscn"
 )
