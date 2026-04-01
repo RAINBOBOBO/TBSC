a2ec921a8365fcaca0497c9e507014a1d2cec304
Rain Babauta <rainbabauta1@gmail.com>
2026-04-01 08:43:49 -0700

    Changing directions
    I decided I'm not going to make the adventuring party manager game first and just make the main game. 2D action roguelite

--- engine/components/career_component.gd
+++
@@ -1,9 +0,0 @@
-class_name CareerComponent extends Resource
-
-@export var current_role: String = "unemployed"
-@export var employer_id: int = -1
-@export var hire_date: int = 0
-@export var salary: int = 0
-@export var contract_duration_days: int = 0
-@export var previous_roles: Array[String] = []
-@export var achievements: Array[String] = []
--- engine/components/company_component.gd
+++
@@ -1,8 +0,0 @@
-class_name CompanyComponent extends Resource
-
-@export var money: int
-@export var active_jobs: Array[int] = []
-@export var completed_jobs: Array[int] = []
-@export var failed_jobs: Array[int] = []
-@export var staff: Array[int] = []
-@export var out_on_job: Array[int] = []
